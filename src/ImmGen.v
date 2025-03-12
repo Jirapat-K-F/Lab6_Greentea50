51f5b92d682ea9e62b22ba3b4e18243547a48df7
Jirapat Kongkatonyoosakul <jirapat.flum@gmail.com>
2025-03-12 14:18:39 +0700

    ImmGen

--- src/ImmGen.v
+++ src/ImmGen.v
@@ -9,7 +9,25 @@ module ImmGen (
         case(opcode)
             // TODO: implement your ImmGen here
             // Hint: follow the RV32I opcode map (table in spec) to set imm value
-            
+            0010011: imm[11:0] = inst[31:20];
+            0000011: imm[11:0] = inst[31:20];
+            0100011: begin 
+                imm[11:5] = inst[31:25];
+                imm[4:0] = inst[11:7];
+            end
+            1100011: begin
+                imm[12] = inst[31];
+                imm[10:5] = inst[30:25];
+                imm[4:1] = inst[11:8];
+                imm[11] = inst[7];
+            end
+            1101111: begin
+                imm[20] = inst[31];
+                imm[10:1] = inst[30:21];
+                imm[11] = inst[20];
+                imm[19:12] = inst[19:12];
+            end
+            1100111: imm[11:0] = inst[31:20];
             
         endcase
     end

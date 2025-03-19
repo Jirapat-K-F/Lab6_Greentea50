e265e745c0d533774b964c543111847439b234dc
Jirapat Kongkatonyoosakul <jirapat.flum@gmail.com>
2025-03-19 12:18:32 +0700

    Add test file and fix immgen

--- src/ImmGen.v
+++ src/ImmGen.v
@@ -9,26 +9,39 @@ module ImmGen (
         case(opcode)
             // TODO: implement your ImmGen here
             // Hint: follow the RV32I opcode map (table in spec) to set imm value
-            0010011: imm[11:0] = inst[31:20];
-            0000011: imm[11:0] = inst[31:20];
+            0010011: begin 
+                imm[31:12] = imm[31];
+                imm[11:0] = inst[31:20];
+            end
+            0000011: begin 
+                imm[31:12] = imm[31];
+                imm[11:0] = inst[31:20];
+            end
             0100011: begin 
+                imm[31:12] = imm[31];
                 imm[11:5] = inst[31:25];
                 imm[4:0] = inst[11:7];
             end
             1100011: begin
+                imm[31:13] = imm[31];
                 imm[12] = inst[31];
+                imm[11] = inst[7];
                 imm[10:5] = inst[30:25];
                 imm[4:1] = inst[11:8];
-                imm[11] = inst[7];
+                imm[0] = 0;
             end
             1101111: begin
+                imm[31:21] = imm[31];
                 imm[20] = inst[31];
-                imm[10:1] = inst[30:21];
-                imm[11] = inst[20];
                 imm[19:12] = inst[19:12];
+                imm[11] = inst[20];
+                imm[10:1] = inst[30:21];
+                imm[0] = 0;
             end
-            1100111: imm[11:0] = inst[31:20];
-            
+            1100111: begin
+                imm[31:12] = imm[31];
+                imm[11:0] = inst[31:20];
+            end
         endcase
     end
 
